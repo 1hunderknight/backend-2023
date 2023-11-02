2feb93d98ee8ed5e5e9096a8585553b991fdf060
orion2182 <admiralyumiko@gmail.com>
2023-11-02 14:19:26 +0700

    tugas pekan 6

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use Illuminate\Http\Request;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Method GET
+# Route animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
-// Method POST
 Route::post('/animals', [AnimalController::class, 'store']);
-
-// Method PUT
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-// Method DELETE
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Route students
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+
+# Method POST
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
